c4a07f85a201c3d9db7edd12e4e898126dfb30c9
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-03-12 12:02:43 +0000

    updated .gitignore, pre-commit, getpj

--- getpj.sh
+++ getpj.sh
@@ -3,11 +3,13 @@
 
 SRC=../node-dictionaries
 
-mkdir -p doc test/app app
+mkdir -p doc config test/app app
 
 cp $SRC/package.json $SRC/.npmignore $SRC/.gitignore $SRC/.jshint* \
     $SRC/README.md $SRC/Gruntfile.js \
     ./
+
+cp $SRC/config/pre-commit config/
 
 cp $SRC/test/setup.js $SRC/test/.jshint* \
     test/
